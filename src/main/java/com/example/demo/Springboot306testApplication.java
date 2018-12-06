4b38a60ad08c6b8098060987c1b9593189b41740
Tiara Milner <tiaramilner@MacBook-Air-5.local>
2018-12-06 01:18:02 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Springboot306testApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot306testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot306testApplication.class, args);
+    }
+}
